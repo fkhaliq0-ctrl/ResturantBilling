a9eb3a57339a143142c48cd9ef7661263821f877
Faisal Khaliq <f.khaliq0@gmail.com>
2026-09-10 15:36:57 +0530

    Export missing audio helper functions to fix build errors

--- src/utils/audio.js
+++ src/utils/audio.js
@@ -1,4 +1,7 @@
-﻿export const playButtonPress = () => { try { const audio = new Audio('https://assets.mixkit.co/active_storage/sfx/2571/2571-preview.mp3'); audio.volume = 0.2; audio.play().catch(()=>{}); } catch(e){} };
-export const playSuccessSound = () => { try { const audio = new Audio('https://assets.mixkit.co/active_storage/sfx/1435/1435-preview.mp3'); audio.volume = 0.3; audio.play().catch(()=>{}); } catch(e){} };
+﻿export const playButtonPress = () => { try { const audio = new Audio('https://assets.mixkit.co/active_storage/sfx/2568/2568-preview.mp3'); audio.volume = 0.3; audio.play().catch(e => {}); } catch(err) {} };
+export const playSuccessSound = () => { try { const audio = new Audio('https://assets.mixkit.co/active_storage/sfx/1435/1435-preview.mp3'); audio.volume = 0.4; audio.play().catch(e => {}); } catch(err) {} };
 export const playCheckoutSuccess = () => playSuccessSound();
-export const playErrorSound = () => { try { const audio = new Audio('https://assets.mixkit.co/active_storage/sfx/2957/2957-preview.mp3'); audio.volume = 0.3; audio.play().catch(()=>{}); } catch(e){} };
+export const playErrorSound = () => { try { const audio = new Audio('https://assets.mixkit.co/active_storage/sfx/2957/2957-preview.mp3'); audio.volume = 0.4; audio.play().catch(e => {}); } catch(err) {} };
+export const playAddSound = () => playButtonPress();
+export const playRemoveSound = () => playButtonPress();
+export const playKeyPress = () => playButtonPress();
