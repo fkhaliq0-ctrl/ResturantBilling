e93c6b0f1a50ed8f1f5f61113547c54e5256e6ac
Faisal Khaliq <f.khaliq0@gmail.com>
2026-09-18 18:27:49 +0530

    Set absolute root base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
   css: {
     transformer: 'postcss',
   },
@@ -18,3 +18,4 @@ export default defineConfig({
     port: 5180,
   },
 })
+
